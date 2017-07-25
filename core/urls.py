7e58576fc23bf3a92646ee6357d7059ffd71449a
tuxu <aomm@cin.ufpe.br>
2017-07-25 22:20:54 +0000

    moving static to the right url

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import url
-from django.conf import settings
-from django.conf.urls.static import static
 from core import views
 
 urlpatterns = [
     url(r'^(?P<slug>[-\w]+)/$', views.ChapterDetail.as_view(), name="chapter_detail"),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
