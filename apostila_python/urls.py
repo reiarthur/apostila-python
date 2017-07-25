87a661c41775ba667d4a715cce6ec2327272facf
tuxu <aomm@cin.ufpe.br>
2017-07-25 22:28:39 +0000

    middleware classes

--- apostila_python/urls.py
+++ apostila_python/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from core import urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
